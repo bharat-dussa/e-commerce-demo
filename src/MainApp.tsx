afab46bd16701972974d511273e8b519b3af281e
Bharat Dussa <bharatdussa@gmail.com>
2024-10-31 14:15:37 +0530

    uncomment code

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import ProductItem from "./ProductItem";
 import CartPage from "./CartPage";
 import NikitaHomePage from "./nikita/HomePage";
-// import HomePage from './home/HomePage'
+import HomePage from './home/HomePage'
 import PoojaHomePage from './pooja/HomePage';
 import SnehaHomePage from "./sneha/HomePage"
 import KeerthanaAboutUsPage from "./Keerthana/aboutUS";
@@ -15,7 +15,7 @@ const MainApp = () => {
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
-            {/* <Route path='/vignesh/home' element={<HomePage />} /> */}
+            <Route path='/vignesh/home' element={<HomePage />} /> 
             <Route path='/product/:id' element={<ProductItem />} />
             <Route path='/cart' element={<CartPage />} />
             <Route path="nikita/homepage" element={<NikitaHomePage />} />
