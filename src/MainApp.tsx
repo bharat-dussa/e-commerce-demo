8c99fa3e828d789d6856b42241ae2a9f6e7675e3
Keerthi <keerthikalva18@gmail.com>
2024-10-31 13:50:13 +0530

    HomePage Done

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,25 +1,25 @@
-
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import ProductItem from "./ProductItem";
 import CartPage from "./CartPage";
 import NikitaHomePage from "./nikita/HomePage";
-import HomePage from './home/HomePage'
 import PoojaHomePage from './pooja/HomePage';
-import SnehaHomePage from "./sneha/HomePage"
+import SnehaHomePage from "./sneha/HomePage";
+import KeerthanaHomePage from "./Keerthana/HomePage"
+
 
 const MainApp = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
-            <Route path='/vignesh/home' element={<HomePage />} />
             <Route path='/product/:id' element={<ProductItem />} />
             <Route path='/cart' element={<CartPage />} />
             <Route path="nikita/homepage" element={<NikitaHomePage />} />
             <Route path="pooja/homepage" element={<PoojaHomePage />} />
             <Route path="sneha/homepage" element={<SnehaHomePage/>}/>
+            <Route path="keerthana/homepage" element={<KeerthanaHomePage/>}/>
 
         </Routes>
     </BrowserRouter>
