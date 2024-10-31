21da941a907e2abdcc73593782a6c48e31796930
vighnesh <vignesh23birru@gmail.com>
2024-10-31 12:18:12 +0530

    MainApp path is vignesh/home is added

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -10,7 +10,7 @@ const MainApp = () => {
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
-            <Route path='/vighnesh/home' element={<HomePage />} />
+            <Route path='/vignesh/home' element={<HomePage />} />
             <Route path='/product/:id' element={<ProductItem />} />
             <Route path='/cart' element={<CartPage />} />
         </Routes>
